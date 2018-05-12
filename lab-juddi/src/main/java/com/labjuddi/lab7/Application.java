9a440a31249208ca234e41a7861ccd9274e1bd74
Prendota <prendota@mail.ru>
2018-05-12 12:35:40 +0300

    add lab7 juddi

---
+++ lab-juddi/src/main/java/com/labjuddi/lab7/Application.java
@@ -0,0 +1,25 @@
+package com.labjuddi.lab7;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Application {
+    
+    public static void main(String[] args) {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            System.out.print("Enter business name: ");
+            String businessName = br.readLine();
+            System.out.print("Enter service name: ");
+            String serviceName = br.readLine();
+            System.out.print("Enter access point url: ");
+            String apoint = br.readLine();
+            JuddiReg.register(businessName, serviceName, apoint);
+            JuddiSearch.search(new String[] {serviceName});
+        }
+        catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
